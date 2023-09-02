7adbfff3819a51445ccfecb164e1f189a42110fb
VastiDev <vastiane1@gmail.com>
2023-09-02 17:41:46 -0300

    starting project

---
+++ src/main/java/com/vastidev/challenge1person/Challenge1PersonApplication.java
@@ -0,0 +1,13 @@
+package com.vastidev.challenge1person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge1PersonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Challenge1PersonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vastidev/challenge1person/Challenge1PersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vastidev.challenge1person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge1PersonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
