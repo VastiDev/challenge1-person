64609e0c91f6deead8ff0520530e42ae12ef6f9f
VastiDev <vastiane1@gmail.com>
2023-09-02 18:36:45 -0300

    implementa api challenge

--- src/main/java/com/vastidev/challenge1person/Challenge1PersonApplication.java
+++ src/main/java/com/vastidev/challenge1person/Challenge1PersonApplication.java
@@ -2,9 +2,19 @@ package com.vastidev.challenge1person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class Challenge1PersonApplication {
+
+    @GetMapping
+    public String getHomeTeste(){
+        return "Challenge1-person API Home";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Challenge1PersonApplication.class, args);
