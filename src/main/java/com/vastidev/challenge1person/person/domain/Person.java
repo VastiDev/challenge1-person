2425f74834c353e69222f1e1b6c044f04dfb141b
VastiDev <vastiane1@gmail.com>
2023-09-02 20:31:22 -0300

    adiciona anotaçoes de tabela

--- src/main/java/com/vastidev/challenge1person/person/domain/Person.java
+++ src/main/java/com/vastidev/challenge1person/person/domain/Person.java
@@ -1,9 +1,8 @@
 package com.vastidev.challenge1person.person.domain;
 
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
@@ -13,19 +12,27 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @Entity
+@Table(name = "Pessoa")
 public class Person {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name= "id", updatable = false, unique = true, nullable = false)
     private UUID idPerson;
+    @Column(name = "Nome completo")
     private String name;
     @NotNull
+    @Column(name = "Data Nascimento")
     private LocalDate birthDate;
     @NotNull
     @Embedded
+    @Column(name = "Endereço Principal")
     private MainAddress main_address;
     @Embedded
+    @Column(name = "Endereço Adicional")
     private AddicionalAddress addicionalAdress;
 
     private LocalDateTime dataHoraDoCadastro;
