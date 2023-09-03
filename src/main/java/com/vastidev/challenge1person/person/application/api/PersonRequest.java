1f2942d0585be641c37f6b9aae1b3453cbedabe2
VastiDev <vastiane1@gmail.com>
2023-09-02 21:54:08 -0300

    implementa request e response

--- src/main/java/com/vastidev/challenge1person/person/application/api/PersonRequest.java
+++ src/main/java/com/vastidev/challenge1person/person/application/api/PersonRequest.java
@@ -1,4 +1,23 @@
 package com.vastidev.challenge1person.person.application.api;
 
+import com.vastidev.challenge1person.person.domain.AddicionalAddress;
+import com.vastidev.challenge1person.person.domain.MainAddress;
+import jakarta.persistence.Embedded;
+import lombok.Value;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDate;
+
+@Value
 public class PersonRequest {
+    private String name;
+    @NotNull
+    private LocalDate birthDate;
+    @NotNull
+    @Embedded
+
+    private MainAddress main_address;
+    @Embedded
+
+    private AddicionalAddress addicionalAdress;
 }
--- src/main/java/com/vastidev/challenge1person/person/application/api/PersonResponse.java
+++ src/main/java/com/vastidev/challenge1person/person/application/api/PersonResponse.java
@@ -1,4 +1,11 @@
 package com.vastidev.challenge1person.person.application.api;
 
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
 public class PersonResponse {
+
+    private UUID idPerson;
 }
